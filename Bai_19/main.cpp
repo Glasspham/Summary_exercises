c4e3c8c11b05dd4c188443949af63c0142bbbee4
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-05 15:49:17 +0700

    Add files via upload

---
+++ Bai_19/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    freopen("E:/Language/Baitaptonghop/Bai_19/MINK.INP", "r", stdin); // Redirects standard input to MINK.INP
+    freopen("E:/Language/Baitaptonghop/Bai_19/MINK.OUT", "w", stdout); // Redirects standard output to MINK.OUT
+    
+    int N, k;
+    cin >> N >> k;
+
+    vector<int> arr(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end()); // Sort the array in ascending order
+
+    cout << arr[k - 1]; // Output the k-th smallest element
+
+    return 0;
+}
