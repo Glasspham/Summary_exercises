74321ec83461bd04f07a9c950ff159b870d4db4d
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-05 15:36:54 +0700

    Add files via upload

---
+++ Bai_14/main.py
@@ -0,0 +1,22 @@
+def solve_fence(n, heights):
+    heights.sort()  # Sắp xếp các độ cao để dễ tính toán
+
+    # Tìm giá trị trung vị (median) của các độ cao
+    median = heights[n // 2]
+
+    # Tính tổng chi phí cần phải trả để đưa các thanh sắt về độ cao trung vị
+    total_cost = sum(abs(height - median) for height in heights)
+
+    return median, total_cost
+
+# Đọc dữ liệu từ file input
+with open('E:\Language\Baitaptonghop\Bai_14\HANGRAO.INP', 'r') as file_in:
+    n = int(file_in.readline().strip())
+    heights = list(map(int, file_in.readline().strip().split()))
+
+# Gọi hàm để giải quyết bài toán
+result_height, min_cost = solve_fence(n, heights)
+
+# Ghi kết quả ra file output
+with open('E:\Language\Baitaptonghop\Bai_14\HANGRAO.OUT', 'w') as file_out:
+    file_out.write(f"{result_height}\n{min_cost}")
