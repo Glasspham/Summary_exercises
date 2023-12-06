e469ee8ae7f0f85ce4448492f5109b0c94d3094e
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-12-06 11:36:47 +0700

    Add files via upload

---
+++ Bai_24/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+// Function to check if a number is prime
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    std::ifstream infile("E:\\Language\\Baitaptonghop\\Bai_24\\DOANNT.INP");
+    std::ofstream outfile("E:\\Language\\Baitaptonghop\\Bai_24\\DOANNT.OUT");
+
+    int Q;
+    infile >> Q;
+    std::vector<int> results;
+
+    for (int q = 0; q < Q; ++q) {
+        int L, R;
+        infile >> L >> R;
+        int count = 0;
+        for (int num = L + 1; num < R; ++num) {
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+        results.push_back(count);
+    }
+
+    for (int result : results) {
+        outfile << result << "\n";
+    }
+
+    infile.close();
+    outfile.close();
+
+    return 0;
+}
