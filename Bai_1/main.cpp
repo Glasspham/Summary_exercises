6ec2659521869729f003ee8ec2f5b80ba613c91b
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-04-01 20:00:36 +0700

    Update main.cpp

--- Bai_1/main.cpp
+++ Bai_1/main.cpp
@@ -1,49 +1,45 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// Chưa tối ưu
-#if 1 // Không dùng function
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        string n;
-        cin >> n;
-        for (char &c : n) 
-            if (c == '0') 
-                c = '5';
-        cout << n << endl;
+#if 0// Chưa tối ưu
+    #if 0 // Không dùng function
+    int main() {
+        int T;
+        cin >> T;
+        while (T--) {
+            string n;
+            cin >> n;
+            for (char &c : n) 
+                if (c == '0') 
+                    c = '5';
+            cout << n << endl;
+        }
+        return 0;
     }
-    return 0;
-}
+    #endif
+
+    #if 0 // Dùng function
+    int t;
+    string n;
+    void solve() {
+        for(int i = 0; i < n.size(); ++i) 
+            if(n[i] == '0') 
+                n[i] = '5';
+        cout << n << '\n';
+    }
+
+    int main() {
+        cin >> t;
+        while(t--) {
+            cin >> n;
+            solve();
+        }
+        return 0;
+    }
+    #endif
 #endif
 
-#if 0 // Dùng function
-int t;
-string n;
-
-void solve() {
-    for(int i = 0; i < n.size(); ++i) 
-        if(n[i] == '0') 
-            n[i] = '5';
-    cout << n << '\n';
-}
-
-
-int main() {
-    cin >> t;
-    while(t--) {
-        cin >> n;
-        solve();
-    }
-    return 0;
-}
-#endif
-
-// Đã Tối ưu
-#include <iostream>
-#include <string>
-using namespace std;
+#if 1 // Đã Tối ưu
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -57,3 +53,4 @@ int main(){
     }
     return 0;
 }
+#endif
