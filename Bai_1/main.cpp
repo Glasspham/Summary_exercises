1b053b5e5998fce57b96aa60577195904bb5455d
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-04-01 19:33:56 +0700

    Update main.cpp

--- Bai_1/main.cpp
+++ Bai_1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+// Chưa tối ưu
 #if 1 // Không dùng function
 int main() {
     int T;
@@ -9,14 +9,11 @@ int main() {
     while (T--) {
         string n;
         cin >> n;
-        for (char &c : n) {
-            if (c == '0') {
+        for (char &c : n) 
+            if (c == '0') 
                 c = '5';
-            }
-        }
         cout << n << endl;
     }
-   
     return 0;
 }
 #endif
@@ -25,28 +22,38 @@ int main() {
 int t;
 string n;
 
-void solve() 
-{
+void solve() {
     for(int i = 0; i < n.size(); ++i) 
-    {
         if(n[i] == '0') 
             n[i] = '5';
-    }
     cout << n << '\n';
 }
 
 
-int main() 
-{
+int main() {
     cin >> t;
-    while(t--) 
-    {
+    while(t--) {
         cin >> n;
         solve();
     }
-    
     return 0;
 }
 #endif
 
-
+// Đã Tối ưu
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n; cin >> n;
+    while(n--){
+        string s; cin >> s;
+        for(char &c : s)
+            if(c == '0')
+                c = '5';
+        cout << s << endl;
+    }
+    return 0;
+}
