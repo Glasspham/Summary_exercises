975a2aae253f290b3c8a1cfb8f33f57e390b70a2
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-04-01 19:13:52 +0700

    Update main.py

--- Bai_1/main.py
+++ Bai_1/main.py
@@ -1,9 +1,14 @@
+# Chưa tối ưu
 T = int(input())
 for _ in range(T):
     n = input().strip()
     n = n.replace('0', '5')
     print(int(n))
 
-input("Nhấn Enter để tiếp tục...")
-
-
+# Tối ưu với số lớn
+from sys import stdin,stdout
+n=int(stdin.readline())
+for i in range(n):
+      t=stdin.readline()
+      t=t.replace('0','5')
+      print(t)
